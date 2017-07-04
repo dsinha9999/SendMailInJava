0838076dfda641b656736772ca5ed3beab3f46ef
dsinha9999 <dsinhad76@gmail.com>
2017-07-04 17:54:12 +0530

    Add files via upload

---
+++ mail.java
@@ -0,0 +1,60 @@
+import java.util.*;
+import javax.mail.*;
+import javax.mail.internet.*;
+
+public class Main {
+
+    private static final String USER_NAME = "abc@gmail.com"; 
+    private static final String PASSWORD = "**********";
+   
+
+    public static void main(String[] args) {
+        String from = USER_NAME;
+        String pass = PASSWORD;
+        String[] to = {"xyz@gmail.com" };
+        String subject = "This will be your subject .";
+        String body = "This will be your message body of mail . ";
+
+        sendFromGMail(from, pass, to, subject, body);
+    }
+
+    private static void sendFromGMail(String from, String pass, String[] to, String subject, String body) {
+        Properties props = System.getProperties();
+        String host = "smtp.gmail.com";
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", host);
+        props.put("mail.smtp.user", from);
+        props.put("mail.smtp.password", pass);
+        props.put("mail.smtp.port", "587");
+        props.put("mail.smtp.auth", "true");
+
+        Session session = Session.getDefaultInstance(props);
+        MimeMessage message = new MimeMessage(session);
+
+        try {
+            message.setFrom(new InternetAddress(from));
+            InternetAddress[] toAddress = new InternetAddress[to.length];
+
+           
+            for( int i = 0; i < to.length; i++ ) {
+                toAddress[i] = new InternetAddress(to[i]);
+            }
+
+            for( int i = 0; i < toAddress.length; i++) {
+                message.addRecipient(Message.RecipientType.TO, toAddress[i]);
+            }
+
+            message.setSubject(subject);
+            message.setText(body);
+            Transport transport = session.getTransport("smtp");
+            transport.connect(host, from, pass);
+            transport.sendMessage(message, message.getAllRecipients());
+            transport.close();
+        }
+        catch (AddressException ae) {
+            ae.printStackTrace();
+        }
+        catch (MessagingException me) {
+            me.printStackTrace();
+        }
+    }}
